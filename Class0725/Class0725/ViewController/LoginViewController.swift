5d9b5d2e7bdfb24cb96d59d324cc563cd1aa80c9
kimdaeman14 <kimdaeman14@gmail.com>
2018-07-25 20:28:28 +0900

    로그인뷰컨트롤러 구현완료

--- Class0725/Class0725/ViewController/LoginViewController.swift
+++ Class0725/Class0725/ViewController/LoginViewController.swift
@@ -7,29 +7,63 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var emailTextField:UITextField!
+    @IBOutlet weak var pwTextField:UITextField!
+    
+    
+    @IBAction private func singUpButton(_ sender: UIButton){
+        print("singUpButton")
+        
+        let url = URL(string: "https://api.lhy.kr/members/profile/")
+        let header : [String : String] = ["Authorization":"Token 855da681de79ea16cdfc31ccd0a1164baae09311"]
+        
+        //여기 필요없는 encoding 이런거 있으면 에러나네.
+        Alamofire.request(url!, method: HTTPMethod.get, parameters: nil, headers: header).validate(statusCode: 200..<400).responseData { (response) in
+            switch response.result{
+            case .success(let value):
+                print(value)
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let navigationViewController = storyboard.instantiateViewController(withIdentifier: "NavigationViewController")
+                navigationViewController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+                self.present(navigationViewController, animated: true)
+                
+            case .failure(let error):
+                print(error)
+                
+                
+            }
+        }
 
-        // Do any additional setup after loading the view.
-    }
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction private func returnMainViewController(_ sender: UIButton){
+        dismiss(animated: true, completion: nil)
+       
     }
-    */
+
+    deinit{
+        print("deinit LoginViewController")
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //키보드는 기본적으로 텍스트필드를 찍으면 올라오긴 하는데,
+        //얘는 뷰시작되자마자 첫응답(텍스트필드를찍을때)가 아니라 그냥 시작되자마자 올라오는 것.
+        emailTextField.becomeFirstResponder()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)//뷰가사라질때 키보드 내려가도록
+    }
+
 
 }
--- Class0725/Class0725/ViewController/MainViewController.swift
+++ Class0725/Class0725/ViewController/MainViewController.swift
@@ -10,26 +10,22 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction private func loginButton(_ sender: UIButton){
+        let storyboard = UIStoryboard(name: "Login", bundle: nil) //띄워지는 스토리보드 이름을 정의해서 넣고
+        //그 스토리보드에있는 뷰컨트롤러 중 어느 뷰에서 어느 뷰로 갈건지 식별자를 지정하고, 프레젠트한다.
+        let loginViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+        loginViewController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+        present(loginViewController, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction private func signUpButton(_ sender: UIButton){
+        let storyboard = UIStoryboard(name: "Login", bundle: nil) //띄워지는 스토리보드 이름을 정의해서 넣고
+        //그 스토리보드에있는 뷰컨트롤러 중 어느 뷰에서 어느 뷰로 갈건지 식별자를 지정하고, 프레젠트한다.
+        let signUpViewController = storyboard.instantiateViewController(withIdentifier: "SignUpViewController")
+        signUpViewController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+        present(signUpViewController, animated: true)
     }
-    */
+    
+    
 
 }
