00616ddc2b26e17a418e9e46ab652e32746da626
kimdaeman14 <kimdaeman14@gmail.com>
2018-07-27 04:58:42 +0900

    포스팅 기능 구현

--- Class0725/Class0725/ViewController/PostViewController.swift
+++ Class0725/Class0725/ViewController/PostViewController.swift
@@ -7,29 +7,53 @@
 //
 
 import UIKit
+import Alamofire
 
 class PostViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField:UITextField!
+    @IBOutlet weak var contentsTextField:UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction private func createPostingButton(_ sender: UIButton) {
+        let url = URL(string: "https://api.lhy.kr/posts/")
+        let params = ["title" :  "\(titleTextField.text ?? "")", "content":  "\(contentsTextField.text ?? "")"]
+        let header = ["Authorization": "Token a88ea26174fa4886c5afd2e1c08a2bb443d766ba"]
+        
+        
+        // FIXME: - token 헤더로 넘기는 방법말고, 회원가입 또는 로그인하면서 하는방법으로 변경하기.
+        // 포스팅하는법 제대로 다시 이해하기
+        // 그리고 사진 재사용되면서 에러발생?하는거? 그거도 해결하기.
+
+        guard let image = UIImage(named: "home") else { return }
+        guard let imageData = UIImagePNGRepresentation(image) else { return }
     
+        Alamofire.upload(multipartFormData: { (multipartFormData) in
+            for (key, value) in params {
+                multipartFormData.append(value.data(using: .utf8)!, withName: key)
+            }
+            multipartFormData.append(imageData,
+                                     withName: "img_cover",
+                                     fileName: "tested.png",
+                                     mimeType: "image/png")
+            
+        }, to: url!, method: HTTPMethod.post, headers: header)
+        { encodingResult in
+            switch encodingResult {
+            case .success(let upload, _, _):
+                upload.responseJSON { response in
+                    print("-------------response--------------")
+                    debugPrint(response)
+                }
+            case .failure(let encodingError):
+                print("---------error---------")
+                print(encodingError)
+            }
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+    }}
