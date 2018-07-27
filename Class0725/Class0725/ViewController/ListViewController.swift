3d11668c8ff8ad72f4bb9cc837d88eb60c3c8528
kimdaeman14 <kimdaeman14@gmail.com>
2018-07-27 19:49:46 +0900

    끊김현상 수정완료

--- Class0725/Class0725/ViewController/ListViewController.swift
+++ Class0725/Class0725/ViewController/ListViewController.swift
@@ -13,8 +13,8 @@ struct PostList: Codable {
     let title: String
     let imageCover: String? //아..이게 null인 경우도 있어서 이거 ? 안해주면 타입이 안맞아서 안뜨나보다.
     let createdDate : String
-//    let author: [Author]
-
+    //    let author: [Author]
+    
     
     enum CodingKeys: String, CodingKey{
         case title
@@ -22,9 +22,9 @@ struct PostList: Codable {
         case createdDate = "created_date"
     }
     
-//    struct Author: Codable {
-//        let name: String
-//    }
+    //    struct Author: Codable {
+    //        let name: String
+    //    }
     
     
 }
@@ -33,14 +33,14 @@ struct PostList: Codable {
 
 class ListViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
-
+    
     var postlist: [PostList] = [] {
         didSet {
             self.tableView.reloadData()
         }
     }
-
-
+    
+    
     @IBAction private func logoutButton(_ sender: UIButton){
         let alertController = UIAlertController(title: "Logout", message:
             "로그아웃 되었습니다.", preferredStyle: UIAlertControllerStyle.alert)
@@ -56,22 +56,22 @@ class ListViewController: UIViewController {
     
     func dataDecode(){
         let url = URL(string: "https://api.lhy.kr/posts/")
-
+        
         Alamofire.request(url!, method: HTTPMethod.get)
             .validate(statusCode: 200..<400)
             .responseData { [weak self] (response) in
-            switch response.result{
-            case .success(let value):
-                print(value)
-                do{
-                    self?.postlist = try JSONDecoder().decode([PostList].self, from: value)
-                }catch{
-                    print(error.localizedDescription)
+                switch response.result{
+                case .success(let value):
+                    print(value)
+                    do{
+                        self?.postlist = try JSONDecoder().decode([PostList].self, from: value)
+                    }catch{
+                        print(error.localizedDescription)
+                    }
+                //                print(self?.postlist)
+                case .failure(let error):
+                    print(error)
                 }
-//                print(self?.postlist)
-            case .failure(let error):
-                print(error)
-            }
         }
     }
     
@@ -80,8 +80,8 @@ class ListViewController: UIViewController {
         super.viewDidLoad()
         dataDecode()
     }
-
-
+    
+    
     
 }
 
@@ -90,41 +90,37 @@ extension ListViewController: UITableViewDataSource{
     
     @available(iOS 2.0, *)
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-
+        
         return postlist.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-//                        print(self.postlist)
-
+        //                        print(self.postlist)
+        
         cell.textLabel?.text = self.postlist[indexPath.row].title
         cell.detailTextLabel?.text = self.postlist[indexPath.row].createdDate
         let imageCoverURL = self.postlist[indexPath.row].imageCover
         
         //URL(string: imageCoverURL)에 값이있는지확인하고 없으면 nil있으면 url에 넣고, 아래 스코프를 실행해라는 명령.
         //아래 스코프를 빈채로두거나 하면 에러남.
-//        URLSession.shared.dataTask(with: <#T##URL#>, completionHandler: <#T##(Data?, URLResponse?, Error?) -> Void#>)
+        //        URLSession.shared.dataTask(with: <#T##URL#>, completionHandler: <#T##(Data?, URLResponse?, Error?) -> Void#>)
         
         //아 행복하다...ㅠㅠ 이래서 옵셔널 바인딩을 배우라는거구나
         if let imageCoverURL = imageCoverURL {
-                    let url = URL(string: imageCoverURL)
+            let url = URL(string: imageCoverURL)
             if let urlData = url {
                 print(urlData, "urlData")
-                    URLSession.shared.dataTask(with: urlData) { (data,respones,error) in
-                        print("URLSession")
-                        //                        print(data, "data")
-                        //                        print(respones, "respones")
-                        //                            if let image = data{
-                        //                            print(image, "image")
-                        //                                let images = UIImage(data: image)
-                        //                                cell.imageView?.image = images
-                        //                            }
-                        //                    let data = try? Data(contentsOf: urlData)
+                let task = URLSession.shared.dataTask(with: urlData) { (data,respones,error) in
+                    if let image = data{
+                        let images = UIImage(data: image)
+                        DispatchQueue.main.async {
+                            cell.imageView?.image = images
+                        }
                     }
-            }else{
-                print("false")
-              
+                    //let data = try? Data(contentsOf: urlData)  //이렇게하면 Data()메소드가 동기방식이라 렉걸린다.
+                }
+                task.resume()
             }
         }
         return cell
